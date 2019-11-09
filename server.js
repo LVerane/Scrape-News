4dcc95ac830f8a4172e3dcad82fafbdc74f917d6
Lucio Verane <verane64@gmail.com>
2019-11-08 20:44:53 -0600

    port fixed

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+var port = process.env.PORT || 3000;
 var exphbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
